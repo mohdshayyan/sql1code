80f0defd20e280bf04f5be934600b0f977b60479
SHAYYAN <127933448+mohdshayyan@users.noreply.github.com>
2023-04-24 18:57:03 +0530

    Delete proj5.py

--- proj5.py
+++
@@ -1,9 +0,0 @@
-'''Write a Pandas program to create and display a DataFrame from a
-specified dictionary with index labels.'''
-import pandas as pd
-import numpy as np
-exam_data = {'name': ['Manish', 'Dhiraj'],
- 'score': [12.5, 9]}
-labels = ['NAME', 'SCORE']
-df = pd.DataFrame(exam_data , index=labels)
-print(df)
