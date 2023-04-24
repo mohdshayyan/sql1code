3fdea65057f68e1760e063d6244432a788e328e3
SHAYYAN <127933448+mohdshayyan@users.noreply.github.com>
2023-04-24 18:55:41 +0530

    Delete newProj20.py

--- newProj20.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-stu_XII={'Name':['Abhay','Bharat','Chandu','Dharmik','Ervin'],
-             'Eng':[66,69,75,74,70],'Maths':[85,82,84,80,75],'IP':[95,82,63,84,90]}
-df=pd.DataFrame(stu_XII)
-df.loc[5]=['Manisha',67,89,45]
-df.loc[6]=['Hemant',77,88,66]
-print(df)
