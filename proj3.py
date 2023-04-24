34931e0c26d29a4588a76d1e36b2a8b074fd979d
SHAYYAN <127933448+mohdshayyan@users.noreply.github.com>
2023-04-24 18:56:26 +0530

    Delete proj3.py

--- proj3.py
+++
@@ -1,8 +0,0 @@
-'''Write a Pandas program to sort a given Series.
-400, 300.12,100, 200'''
-import pandas as pd
-s = pd.Series([400, 300.12,100, 200])
-print("Original Data Series:")
-print(s)
-new_s = pd.Series(s).sort_values()
-print(new_s)
